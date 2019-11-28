870ebc98b7486517b799b94e8df2c8e8ccac9482
eduvieagada <eduvieagada@gmail.com>
2019-11-28 14:22:10 +0100

    added spring boot dev tools

--- stockapplication-web/src/main/java/com/payconiq/web/StockApplication.java
+++ stockapplication-web/src/main/java/com/payconiq/web/StockApplication.java
@@ -1,36 +1,13 @@
 package com.payconiq.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.thymeleaf.templateresolver.FileTemplateResolver;
-import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.payconiq.*")
 public class StockApplication {
-
-    @Autowired
-    private ThymeleafProperties properties;
-
-    @Value("${spring.thymeleaf.templates_root:}")
-    private String templatesRoot;
-
     public static void main(String[] args) {
         SpringApplication.run(StockApplication.class, args);
-    }
-
-    @Bean
-    public ITemplateResolver defaultTemplateResolver() {
-        FileTemplateResolver resolver = new FileTemplateResolver();
-        resolver.setSuffix(properties.getSuffix());
-        resolver.setPrefix(templatesRoot);
-        resolver.setTemplateMode(properties.getMode());
-        resolver.setCacheable(properties.isCache());
-        return resolver;
     }
 }
